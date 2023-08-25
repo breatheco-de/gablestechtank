f84ebb0d451769e44dd621b565fc79a263ba1ea4
Alejandro Sanchez <aalejo@gmail.com>
2023-08-25 02:04:04 -0400

    Update MktRecommendedCourses.jsx

--- src/common/components/MktRecommendedCourses.jsx
+++ src/common/components/MktRecommendedCourses.jsx
@@ -24,7 +24,7 @@ function MktRecommendedCourses({ id, technologies, background, title, gridColumn
   const [courses, setCourses] = useState([]);
   const { hexColor, fontColor, featuredColor } = useStyle();
 
-  const defaultHostAndEndpoint = `${BREATHECODE_HOST}/v1/marketing/course?$academy={WHITE_LABEL_ACADEMY}`;
+  const defaultHostAndEndpoint = `${BREATHECODE_HOST}/v1/marketing/course?academy=${WHITE_LABEL_ACADEMY}`;
 
   const headers = {
     'Accept-Language': lang,
